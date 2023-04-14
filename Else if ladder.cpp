28bb2e913e382076153581e069e05225bda85f49
Sudhansu14 <130463585+Sudhansu14@users.noreply.github.com>
2023-04-14 17:34:06 +0530

    Add files via upload

---
+++ Else if ladder.cpp
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+main()
+{
+	int a;
+	
+	    printf("value of A=");
+		scanf("%d=",&a);
+		
+		if(a<0)
+		{
+			printf("A is positive");	
+		}
+	    else if (a==0)
+		{
+			printf("A is nutral");
+		} 
+	    else if (a>0)
+	    {
+	    	printf("A is negetive");
+		}
+}
---
+++ IF ELSE.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+main()
+{
+	int a=600,b=200;
+	
+	if (a<b)
+	
+	{
+		printf("a is minimum");
+		
+	}
+	
+	else
+	 
+	 {
+	 	printf("b is minimum"); 
+	 	
+	 }
+}
